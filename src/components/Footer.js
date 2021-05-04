4d89275862fb95633e3c38e29900232dbdd44cab
MatthiasChometon <matthias.chometon@ynov.com>
2021-05-04 15:40:19 +0100

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <nav className='NavBar' class="navbar navbar-light bg-light">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </nav>
+        );
+    }
+}
+
+export default Footer;
